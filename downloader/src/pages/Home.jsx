aa6b6f6bab8d9223740ab34ad3877b5a7d803e26
Zasan <freefiregpgamer4@gmail.com>
2026-09-19 22:35:09 +0600

    Use environment variable for API URL

--- downloader/src/pages/Home.jsx
+++ downloader/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import SEO from '../../components/SEO';
 import './Home.css';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL;
 
 function formatDuration(duration) {
     const totalSeconds = Number(duration);
